ca265a556bd1ff1a46faa203a97a6a9947378486
juan <jaromero6@uc.cl>
2019-07-19 18:06:44 -0400

    :sparkles: Created classes

---
+++ modules/classes/organizer.js
@@ -0,0 +1,7 @@
+export class Organizer{
+    constructor(technicians, bosses, modules){
+        this.technicians = technicians;
+        this.bosses = bosses;
+        this.modules = modules;
+    }
+}
