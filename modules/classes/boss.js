6dffa5a27a39a9f3fa83c4c7fd81d8ec22fdf57a
juan <jaromero6@uc.cl>
2019-07-20 17:56:48 -0400

    :construction: :bug: Solved some busgs and get the feasible time modules of Bosses

--- modules/classes/boss.js
+++ modules/classes/boss.js
@@ -1,8 +1,8 @@
-class Boss{
+export class Boss{
     constructor(name, modules, capacity){
         this.name = name;
         this.modules = modules;
         this.maxModules = capacity;
-        this.assigned_modules = array();
+        this.assigned_modules = Array();
     }
 }
--- modules/classes/organizer.js
+++ modules/classes/organizer.js
@@ -1,7 +1,20 @@
+
+'use strict';
 export class Organizer{
     constructor(technicians, bosses, modules){
         this.technicians = technicians;
         this.bosses = bosses;
         this.modules = modules;
+    }
+
+    getBossesModules(){
+        let modulesSet = new Set();
+        for(let i = 0; i < this.bosses.length; i++){
+            let currentModules = this.bosses[i].modules;
+            for(let j = 0; j < currentModules.length; j++){
+                modulesSet.add(currentModules[j])
+            }
+        }
+        return modulesSet;
     }
 }
