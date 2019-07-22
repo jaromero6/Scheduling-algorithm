06ca762b38f94f05d3cd772e9a95e5c784c9ab13
juan <jaromero6@uc.cl>
2019-07-22 13:25:09 -0400

    :sparkles: :white_check_mark:  Phase I completed

---
+++ modules/functions/handle.js
@@ -0,0 +1,23 @@
+'use strict';
+import {Organizer} from '../classes/organizer.js';
+
+
+export async function createOrganizer(bosses, technicians){
+    let organizer = new Organizer();
+    for(let i = 0; i < bosses.length; i++){
+        let idBoss = bosses[i]['idBoss'];
+        let name = bosses[i]['name'];
+        let modules = bosses[i]['modules'];
+        let capacity = bosses[i]['capacity'];
+        organizer.addBoss(idBoss, name, modules, capacity);
+    }
+    for(let i = 0; i < technicians.length;i++){
+        let idTehnician =technicians[i]['idTechnician'];
+        let name = technicians[i]['name'];
+        let modules = technicians[i]['modules'];
+        organizer.addTechnician(idTehnician, name, modules);
+    }
+    organizer.removeRedundantBosses();
+    return organizer;
+
+}
